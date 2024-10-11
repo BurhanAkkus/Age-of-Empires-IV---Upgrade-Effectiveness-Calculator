f0e9f5142f2d97e1747252dd3d397af3872b76ee
Burhan Akkus <burgcanmail@gmail.com>
2024-10-11 22:11:13 +0200

    Units now track applied upgrades

--- unit.py
+++ unit.py
@@ -11,6 +11,7 @@ class Unit:
         """
         self.unit_name = unit_name
         self.lookup_stats()
+        self.applied_upgrades = []  # List to track applied upgrades
 
     @classmethod
     def load_unit_stats(cls):
@@ -34,20 +35,44 @@ class Unit:
             self.ranged_armor = stats['ranged_armor']
             self.cost = stats['cost']
         else:
-            raise ValueError(f"Unit '{self.unit_name}' not found in stats file.")\
-
+            raise ValueError(f"Unit '{self.unit_name}' not found in stats file.")
 
     def apply_upgrade(self, upgrade):
         """
-        Applies the bonuses from the upgrade to the unit.
+        Applies the bonuses from the upgrade to the unit and tracks the applied upgrade.
 
         Parameters:
         upgrade (Upgrade): The upgrade to be applied to the unit.
         """
+        if self.has_upgrade_been_applied(upgrade):
+            raise ValueError(f"Upgrade '{upgrade.upgrade_name}' has already been applied to unit '{self.unit_name}'.")
+
         if upgrade.unit_name is None or upgrade.unit_name == self.unit_name:
             self.hp += upgrade.hp_bonus
             self.attack_damage += upgrade.attack_bonus
             self.melee_armor += upgrade.melee_armor_bonus
             self.ranged_armor += upgrade.ranged_armor_bonus
+            self.applied_upgrades.append(upgrade.upgrade_name)  # Track the applied upgrade
         else:
             raise ValueError(f"Upgrade '{upgrade.upgrade_name}' is not applicable to unit '{self.unit_name}'.")
+
+    def list_applied_upgrades(self):
+        """
+        Returns a list of applied upgrades.
+
+        Returns:
+        list: A list of upgrade names that have been applied to the unit.
+        """
+        return self.applied_upgrades
+
+    def has_upgrade_been_applied(self, upgrade):
+        """
+        Checks if the given upgrade has been applied to the unit.
+
+        Parameters:
+        upgrade (Upgrade): The upgrade to check.
+
+        Returns:
+        bool: True if the upgrade has been applied, False otherwise.
+        """
+        return upgrade.upgrade_name in self.applied_upgrades
